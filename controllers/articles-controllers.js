52af2cb659c53c68be32bf7081198242dd776e21
Mchumbles <lewis.mcguire92@yahoo.com>
2024-10-15 12:18:26 +0100

    add new endpoint /api/articles with sort_by functionality, with tests

--- controllers/articles-controllers.js
+++ controllers/articles-controllers.js
@@ -1,5 +1,20 @@
-const { selectArticleById } = require("../models/articles-models");
+const {
+  selectArticleById,
+  selectArticles,
+} = require("../models/articles-models");
 const db = require("../db/connection");
+const { request } = require("express");
+
+exports.getArticles = (request, response, next) => {
+  const { sort_by } = request.query;
+  selectArticles(sort_by)
+    .then((articles) => {
+      response.status(200).send({ articles });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
 
 exports.getArticleById = (request, response, next) => {
   const { article_id } = request.params;

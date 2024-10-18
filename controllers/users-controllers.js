978df915455148b4083e61910f979a5602202dba
Mchumbles <lewis.mcguire92@yahoo.com>
2024-10-18 13:46:13 +0100

    Implement GET for path /api/users/:username with tests

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,4 +1,4 @@
-const { selectUsers } = require("../models/users-models");
+const { selectUsers, selectUserByUsername } = require("../models/users-models");
 const db = require("../db/connection");
 const { request } = require("express");
 
@@ -6,6 +6,20 @@ exports.getUsers = (request, response, next) => {
   selectUsers()
     .then((users) => {
       response.status(200).send({ users });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
+
+exports.getUserByUsername = (request, response, next) => {
+  const { username } = request.params;
+  selectUserByUsername(username)
+    .then((user) => {
+      if (user === undefined) {
+        response.status(200).send({ msg: "user not found" });
+      }
+      response.status(200).send({ user });
     })
     .catch((error) => {
       next(error);
--- models/users-models.js
+++ models/users-models.js
@@ -5,3 +5,16 @@ exports.selectUsers = () => {
     return result.rows;
   });
 };
+
+exports.selectUserByUsername = (username) => {
+  return db
+    .query(
+      `
+    SELECT * FROM users
+    WHERE username = $1`,
+      [username]
+    )
+    .then((user) => {
+      return user.rows[0];
+    });
+};
